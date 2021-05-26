45101a1330f86ddfaf2398ec04730cf32415a4cc
wuyong <lingshizhuangzi@gmail.com>
2021-05-26 23:08:56 +0800

    fix: 修复.gitignore未生效问题

--- Example/Pods/Target Support Files/FWDebug/FWDebug-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_FWDebug : NSObject
-@end
-@implementation PodsDummy_FWDebug
-@end
--- Example/Pods/Target Support Files/NWPusher/NWPusher-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NWPusher : NSObject
-@end
-@implementation PodsDummy_NWPusher
-@end
