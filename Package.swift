8b6e2690766ee1537d4febdda34b5bfa0e13480d
wuyong <lingshizhuangzi@gmail.com>
2024-04-26 22:49:51 +0800

    chore: 5.3.0版本

--- Package.swift
+++ Package.swift
@@ -1,32 +1,22 @@
-// swift-tools-version:5.9
-
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "FWDebugPackage",
-    platforms: [
-        .iOS("11.0")
-    ],
+    name: "FWDebug",
+        platforms: [
+            .iOS(.v12)
+        ],
     products: [
         .library(
-            name: "FWDebugPackage",
-            targets: ["FWDebugPackage"]
-        ),
-    ],
-    dependencies: [
+            name: "FWDebug",
+            targets: ["FWDebug"]
+        )
     ],
     targets: [
-        .target(
-            name: "FWDebugPackage",
-            dependencies: [
-                .target(name: "FWDebug"),
-            ],
-            path: "Package"
-        ),
         .binaryTarget(
             name: "FWDebug",
             url: "https://github.com/lszzy/FWDebug/releases/download/5.3.0/FWDebug.xcframework.zip",
             checksum: "c2941ebe998c381bdacf50d635eeef5ab72edb8c3992d987525cd06cb23ae7d7"
-        ),
+        )
     ]
 )
