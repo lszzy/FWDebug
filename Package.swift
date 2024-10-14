a5d9f01fcb023366b30818dac11e802bb9e8475c
wuyong <lingshizhuangzi@gmail.com>
2024-10-14 16:11:02 +0800

    chore: 6.0.0版本

--- Package.swift
+++ Package.swift
@@ -15,8 +15,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "FWDebug",
-            url: "https://github.com/lszzy/FWDebug/releases/download/5.5.0/FWDebug.xcframework.zip",
-            checksum: "9e5b06907e54adf494754cb61f334e52029555bab8dc938a29521acf2293faa2"
+            url: "https://github.com/lszzy/FWDebug/releases/download/6.0.0/FWDebug.xcframework.zip",
+            checksum: "30da5a8a975e2d865a34b771e445aa34ee3e2adebdf5877286ea91294db879a4"
         )
     ]
 )
